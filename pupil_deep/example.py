496f08531890339b334d9ababce2443a51c2f6ed
Marcos <marcos.v.silva@live.com>
2019-12-27 00:03:47 -0300

    Create class process movies

--- pupil_deep/example.py
+++ pupil_deep/example.py
@@ -4,66 +4,82 @@ import os
 import numpy as np
 
 
-def pupil_process(exam):
-    eye_tracker = deepeye.DeepEye()
-    number_frame = 0
+class Main:
+    def __init__(self):
+        self.dataset_path = r'C:\Projects\Datasets\eye_test\movies'
+        self.dataset_out = r'C:\Projects\Datasets\eye_test\out'
+        self.dataset_label = r'C:\Projects\Datasets\eye_test\label'
 
-    while True:
-        _, frame = exam.read()
+        self.title = ''
+        self.dataset_out_exam = ''
 
-        if frame is None:
-            break
+        self.eye_tracker = deepeye.DeepEye()
 
-        original = np.copy(frame[:, :, 0])
+    def add_label(self, information):
+        with open(r'{}\{}_label.csv'.format(self.dataset_label, self.title), 'a', newline='') as file:
+            file.write('{}\n'.format(information))
+            file.close()
 
-        yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
-        yuv[:, :, 0] = cv2.equalizeHist(yuv[:, :, 0])
-        bgr = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR)
+    def pupil_process(self, exam):
+        number_frame = 0
 
-        gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
-        gaussian = cv2.GaussianBlur(gray, (9, 9), 3)
-        median = cv2.medianBlur(gaussian, 3)
+        while True:
+            _, frame = exam.read()
 
-        kernel = np.ones((5, 5), np.uint8)
-        erode = cv2.erode(median, kernel=kernel, iterations=1)
-        dilate = cv2.dilate(erode, kernel=kernel, iterations=1)
+            if frame is None:
+                break
 
-        #tf.placeholder(tf.float32, [None, 28, 28, 1])
+            original = np.copy(frame[:, :, 0])
 
-        position = eye_tracker.run(original)
+            yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+            yuv[:, :, 0] = cv2.equalizeHist(yuv[:, :, 0])
+            bgr = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR)
 
-        lin, col = gray.shape
-        if 0 < position[0] < lin and 0 < position[1] < col:
-            cv2.circle(original, (int(position[0]), int(position[1])), 10, (255, 255, 0), 2)
+            gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+            gaussian = cv2.GaussianBlur(gray, (9, 9), 3)
+            median = cv2.medianBlur(gaussian, 3)
 
-        text = 'frame={}, x={}, y={}'.format(number_frame, position[0], position[1])
-        cv2.putText(original, text, (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+            kernel = np.ones((5, 5), np.uint8)
+            erode = cv2.erode(median, kernel=kernel, iterations=1)
+            dilate = cv2.dilate(erode, kernel=kernel, iterations=1)
 
-        number_frame += 1
-        path_out = r'{}\{}.png'.format(dataset_out, number_frame)
+            # tf.placeholder(tf.float32, [None, 28, 28, 1])
 
-        cv2.namedWindow('Analysis', cv2.WINDOW_NORMAL)
-        cv2.imshow('Analysis', original)
-        cv2.waitKey(1)
-        cv2.imwrite(path_out, original)
+            position = self.eye_tracker.run(original)
 
-        with open(dataset_label, 'a') as csvfile:
-            csvfile.write("{},{},{}\n".format(number_frame, position[0], position[1]))
-            csvfile.close()
+            lin, col = gray.shape
+            if 0 < position[0] < lin and 0 < position[1] < col:
+                cv2.circle(original, (int(position[0]), int(position[1])), 10, (255, 255, 0), 2)
 
-    exam.release()
-    cv2.destroyAllWindows
+            text = 'frame={}, x={}, y={}'.format(number_frame, position[0], position[1])
+            cv2.putText(original, text, (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+
+            number_frame += 1
+            path_out = r'{}\{}.png'.format(self.dataset_out_exam, number_frame)
+
+            cv2.namedWindow('Analysis', cv2.WINDOW_NORMAL)
+            cv2.imshow('Analysis', original)
+            cv2.waitKey(1)
+            cv2.imwrite(path_out, original)
+
+            self.add_label("{},{},{}".format(number_frame, position[0], position[1]))
+
+        exam.release()
+        cv2.destroyAllWindows
+
+    def run(self):
+        files = os.listdir(self.dataset_path)
+        for file in files:
+            self.title = file.replace('.mp4', '')
+            self.add_label('frame,x,y')
+
+            self.dataset_out_exam = r'{}\{}'.format(self.dataset_out, self.title)
+            os.mkdir(self.dataset_out_exam)
+
+            exam = cv2.VideoCapture('{}/{}'.format(self.dataset_path, file))
+            self.pupil_process(exam)
 
 
-dataset_path = r'C:\Projects\Datasets\eye_test\movies'
-dataset_out = r'C:\Projects\Datasets\eye_test\out'
-dataset_label = r'C:\Projects\Datasets\eye_test\label\dataset.csv'
-exams = os.listdir(dataset_path)
-
-with open(dataset_label, 'a', newline='') as csvfile:
-    csvfile.write("frame,x,y\n")
-    csvfile.close()
-
-for exam in exams:
-    movie = cv2.VideoCapture('{}/{}'.format(dataset_path, exam))
-    pupil_process(movie)
+main = Main()
+if __name__ == '__main__':
+    main.run()
