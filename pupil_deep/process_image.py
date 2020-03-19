c6dd37eb2ea03d1470b038c78d36cb84166e8e52
Marcos Vinicius <marcos.v.silva@live.com>
2020-03-18 23:26:38 -0300

    Add new functions

--- pupil_deep/process_image.py
+++ pupil_deep/process_image.py
@@ -4,14 +4,15 @@ import numpy as np
 
 class ProcessImage:
     def __init__(self):
-        # Params
 
+        # Params
         self._alpha = 1.0
         self._alpha_max = 500
         self._beta = 0
         self._beta_max = 200
         self._gamma = 1.0
         self._gamma_max = 200
+        self._max_recursion = 10
 
         # Brightness Params
         self._range_accept_brightness = 10
@@ -21,7 +22,7 @@ class ProcessImage:
         # Variables
         self._mean_color = np.array([])
 
-    def _pre_process(self, frame, gray=False):
+    def _pre_process(self, frame):
         yuv = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
         yuv[:, :, 0] = cv2.equalizeHist(yuv[:, :, 0])
         bgr = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR)
@@ -34,30 +35,32 @@ class ProcessImage:
         erode = cv2.erode(median, kernel=kernel, iterations=1)
         return cv2.dilate(erode, kernel=kernel, iterations=1)
 
-    def _gamma_corret_image(self, image, gamma):
-        look_table = np.empty((1, 256), np.uint8)
-        for i in range(256):
-            look_table[0, i] = np.clip(pow(i / 255.0, gamma) * 255.0, 0, 255)
+    def _gamma_correction(self, img_original, gamma):
+        inv_gamma = 1.0 / gamma
+        table = np.array([((i / 255.0) ** inv_gamma) * 255
+                          for i in np.arange(0, 256)]).astype("uint8")
 
-        return cv2.LUT(image, look_table)
+        return cv2.LUT(img_original, table)
 
     def _add_brightness(self, image):
-        return cv2.convertScaleAbs(image, alpha=1.0, beta=10)
+        return self._gamma_correction(image, gamma=1.3)
 
     def _rem_brightness(self, image):
-        return cv2.convertScaleAbs(image, alpha=1.0, beta=-10)
+        return self._gamma_correction(image, gamma=0.7)
 
     def _treat_brightness(self, img_process, img_original):
-        mean_image = int(self._mean_color.mean())
-        range_min, range_max = mean_image - self._range_accept_brightness, mean_image + self._range_accept_brightness
-        if int(np.array(img_process).mean()) not in range(range_min, range_max, 1):
-            if int(np.array(img_process).mean()) >= range_max:
-                new_image = self._rem_brightness(img_original)
-            else:
-                new_image = self._add_brightness(img_original)
+        mean_images = int(self._mean_color.mean())
+        range_limit = range(mean_images - self._range_accept_brightness, mean_images + self._range_accept_brightness, 1)
 
-            new_process = self._pre_process(new_image, gray=True)
-            img_process = self._treat_brightness(new_image, new_process)
+        recursion = 0
+        while (int(np.array(img_original).mean()) not in range_limit) and (recursion < self._max_recursion):
+            if int(np.array(img_original).mean()) >= max(range_limit):
+                img_original = self._rem_brightness(img_original)
+            else:
+                img_original = self._add_brightness(img_original)
+
+            img_process = self._pre_process(img_original)
+            recursion += 1
 
         return img_process
 
@@ -65,11 +68,12 @@ class ProcessImage:
         if img_original is None:
             return 0
 
+        if len(self._mean_color) < self._max_size_list_color:
+            self._mean_color = np.append(self._mean_color, np.array(img_original).mean())
+
         img_process = self._pre_process(img_original)
 
-        if len(self._mean_color) < self._max_size_list_color:
-            self._mean_color = np.append(self._mean_color, np.array(img_process).mean())
-
+        self._recursion = 0
         if len(self._mean_color) >= self._min_size_list_color_process:
             img_process = self._treat_brightness(img_process, img_original)
 
